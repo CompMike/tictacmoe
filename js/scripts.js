604f773e9b3682a5a0acf2f64a035eaaccba6b05
Michael Heard <compmike@gmail.com>
2015-01-20 20:54:29 -0500

    framed out js and made newgame reset buttons functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 //Create a javascript tic tac toe game
-
-
+$( document ).ready(function() {
 //find the current box checked
 //compare it to the other boxes
-
 var $currentSquare = $("");
+//create a 9x9 board with a position selected to start the game.
+//Watch for a position to be selected
+//When it's selected add in the computers selection
+//loop the above until somone wins or there's a tie
+
+	function render() {
+		var startingBlock = Math.floor(Math.random()*8);
+		console.log(startingBlock);
+		for (var i = 0; i < 9; i++) {
+			$("div.board").append("<div class='option'>" + i + "</div>");
+			console.log(i);
+		};
+	}
+
+
+$("#newgame").on("click", function(){
+	console.log("click");
+	render();
+});
+
+});
