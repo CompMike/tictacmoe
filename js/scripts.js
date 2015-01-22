852b7cad6cd2f0572ae21a6a64bd70ab875e3277
Michael Heard <michaelheard@RSG-2499-Michael-Heard-MB-Pro.local>
2015-01-21 20:21:18 -0500

    misc changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,9 @@ $( document ).ready(function() {
 //Watch for a position to be selected
 //When it's selected add in the computers selection
 //loop the above until somone wins or there's a tie
-
+var board = ["###",
+						 "###",
+						 "###"];
 	function renderBoard() {
 		//set the first X on the board
 		var startingPos = Math.floor(Math.random()*9);
@@ -14,17 +16,17 @@ $( document ).ready(function() {
 		//loop through 9 tiles until the board is complete
 		for (var i = 0; i < 9; i++) {
 			console.log(i);
+			var box = "box" + i;
 			//Add the startingPos in one of the squares
 			if (startingPos === i) {
-				$("div.board").append("<div class='option' id='box" + i + "'>X</div>");
+				var box = "box" + i;
+				$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'>X</label><input type='checkbox' id='box" + i + "'>");
 			} else {
-				$("div.board").append("<div class='option' id='box" + i + "'></div>");
+				$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
 			}
 			//when a box is clicked add a check in another random square unselected square
 		};
 	}
-
-
 
 $("#newgame").on("click", function(){
 	console.log("click");
