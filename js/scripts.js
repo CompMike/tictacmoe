6231b5e1b7f5adf689eef01969cfbf953e00bb8d
Michael Heard <michaelheard@RSG-2499-Michael-Heard-MB-Pro.local>
2015-01-22 18:00:34 -0500

    added some image files and moded js

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,24 @@ var board = ["###",
 						 "###",
 						 "###"];
 var boardAnswers = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
-	function renderBoard() {
-		//set the first X on the board
-		var startingPos = Math.floor(Math.random()*9);
-		console.log(startingPos);
-		//loop through 9 tiles until the board is complete
-		for (var i = 0; i < 9; i++) {
-			console.log(i);
+function renderBoard() {
+	//set the first X on the board
+	var startingPos = Math.floor(Math.random()*9);
+	console.log(startingPos);
+	//loop through 9 tiles until the board is complete
+	for (var i = 0; i < 9; i++) {
+		console.log(i);
+		var box = "box" + i;
+		//Add the startingPos in one of the squares
+		if (startingPos === i) {
 			var box = "box" + i;
-			//Add the startingPos in one of the squares
-			if (startingPos === i) {
-				var box = "box" + i;
-				$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'>X</label><input type='checkbox' id='box" + i + "'>");
-			} else {
-				$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
-			}
-			//when a box is clicked add a check in another random square unselected square
-		};
-	}
+			$("div.board").append("<label class='option x' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
+		} else {
+			$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
+		}
+		//when a box is clicked add a check in another random square unselected square
+	};
+}
 
 $("#newgame").on("click", function(){
 	console.log("click");
