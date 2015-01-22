f58c794f58dad33775e45813d185f04cdf6bf7d1
Michael Heard <compmike@gmail.com>
2015-01-22 08:10:59 -0500

    added board answers

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ $( document ).ready(function() {
 var board = ["###",
 						 "###",
 						 "###"];
+var boardAnswers = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
 	function renderBoard() {
 		//set the first X on the board
 		var startingPos = Math.floor(Math.random()*9);
