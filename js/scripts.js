96a85e08115fbd02f8ffcba1a89144679f182e19
Michael Heard <michaelheard@RSG-2499-Michael-Heard-MB-Pro.local>
2015-01-23 12:55:13 -0500

    worked on for loops and logic on comparing playerboards

--- js/scripts.js
+++ js/scripts.js
@@ -28,24 +28,37 @@ $( document ).ready(function() {
 				$("div.board").append("<label class='option' for='" + box + "' id='" + box +"'></label><input type='radio' id='" + box + "'>");
 			}
 		};
+	//functions to check state of boards
+	var checkBoards = function(playerBoardState,winCondition) {
+		// console.log(playerBoardState);
+		for (var i = 0; i < winCondition.length; i++) {
+			console.log(winCondition[i]);
+			var count = 0;
+			for (var j = 0; j < winCondition[i].length; j++) {
+				console.log(playerBoard[j] + "===" + winCondition[i][j]);
+				if ($.inArray(playerBoard[j],winCondition[i]) != -1) {
+					console.log("in the array");
+					count++;
+				} ;
+				// if (winCondition[i][j] === playerBoard[j]) {
+				// 	count++;
+				// 	console.log(count);
+				// };
+			};
+			if(count > 2) {
+				console.log("win");
+			}
+		};
+
+	}
 	//function to check the status of the board and break if someone wins
 	var checkStatus = function() {
-		for (var i = winningBoards.length - 1; i >= 0; i--) {
-			if (_.contains(winningBoards[i],playerBoard)) {
-				console.log("yes");
-			};
-		};
 		// for (var i = winningBoards.length - 1; i >= 0; i--) {
-		// 	winningBoards[i].indexOf(playerBoard);
+		// 	if (_.contains(winningBoards[i],playerBoard)) {
+		// 		console.log("yes");
+		// 	};
 		// };
-		// var idx = winningBoards.indexOf(playerBoard);
-		// while (idx != 1) {
-
-		// }
-		// if ($.inArray(playerBoard,winningBoards)) {
-		// 	//add X to non occupied spot
-		// 	console.log('win');
-		// }
+		checkBoards(compBoard,winningBoards);
 	};
 	//function for each move
 	var move = function() {
