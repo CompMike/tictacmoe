e3b598cf2eeda4d97fc34172555862fb6f35366f
Michael Heard <compmike@gmail.com>
2015-01-22 22:24:43 -0500

    created boards for keeping track of player, computer and complete board"

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,60 @@
 //Create a javascript tic tac toe game
 $( document ).ready(function() {
-//find the current box checked
-//compare it to the other boxes
-//create a 9x9 board with a position selected to start the game.
-//Watch for a position to be selected
-//When it's selected add in the computers selection
-//loop the above until somone wins or there's a tie
-var board = ["###",
-						 "###",
-						 "###"];
-var boardAnswers = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
-function renderBoard() {
-	//set the first X on the board
-	var startingPos = Math.floor(Math.random()*9);
-	console.log(startingPos);
-	//loop through 9 tiles until the board is complete
-	for (var i = 0; i < 9; i++) {
-		console.log(i);
-		var box = "box" + i;
-		//Add the startingPos in one of the squares
-		if (startingPos === i) {
+	var playerBoard = [];
+	var compBoard = [];
+	var completeBoard = [];
+	//To win the board must contain the combinations below
+	var winningBoards = [["box0","box1","box2"],
+											["box3","box4","box5"],
+											["box6","box7","box8"],
+											["box0","box4","box8"],
+											["box2","box4","box6"],
+											["box0","box3","box6"],
+											["box1","box4","box7"],
+											["box2","box5","box8"]];
+	var renderBoard = function() {
+		//set the first X on the board
+		var startingPos = Math.floor(Math.random()*9);
+		//add startingPos to boards
+		compBoard.push("box"+startingPos);
+		completeBoard.push("box"+startingPos);
+		//loop through 9 tiles until the board is complete
+		for (var i = 0; i < 9; i++) {
 			var box = "box" + i;
-			$("div.board").append("<label class='option x' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
-		} else {
-			$("div.board").append("<label class='option' for='box" + i + "' id='box" + i + "'></label><input type='checkbox' id='box" + i + "'>");
-		}
-		//when a box is clicked add a check in another random square unselected square
+			//Add the startingPos in one of the squares
+			if (startingPos === i) {
+				$("div.board").append("<label class='option x selected' for='" + box + "' id='" + box +"'></label><input type='radio' id='" + box + "'>");
+			} else {
+				$("div.board").append("<label class='option' for='" + box + "' id='" + box +"'></label><input type='radio' id='" + box + "'>");
+			}
+		};
+	//function to check the status of the board and break if someone wins
+	var checkStatus = function() {
+
 	};
+	//function for each move
+	var move = function() {
+		var $this = $(this);
+			//if already selected don't add selection
+		if (!$this.hasClass("selected")) {
+			$this.addClass("o").addClass("selected");
+		};
+		//add current selection to board
+		var current = $this.get(0).id;
+		playerBoard.push(current);
+		completeBoard.push(current);
+		console.log("playerboard [" + playerBoard + "]");
+		console.log("computerboard [" + compBoard + "]");
+		console.log("completeboard [" + completeBoard + "]");
+	}
+	//when a box is clicked add a check in another random square unselected square
+	$(".option").on("click", move);
+
 }
 
 $("#newgame").on("click", function(){
-	console.log("click");
+
 });
 
-	renderBoard();
+renderBoard();
 });
